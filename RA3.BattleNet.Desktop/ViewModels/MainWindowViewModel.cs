c5772fc49fe220d81783d569eb217777afb5bf43
MrBBBaiXue <mrbbbaixue@bakneko.com>
2022-11-10 22:58:56 +0800

    Update LibGitSharp

--- RA3.BattleNet.Desktop/ViewModels/MainWindowViewModel.cs
+++ RA3.BattleNet.Desktop/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,26 @@
-﻿namespace RA3.BattleNet.Desktop.ViewModels
+﻿using System;
+using System.IO;
+using Avalonia.Controls.Shapes;
+using LibGit2Sharp;
+using Path = System.IO.Path;
+
+namespace RA3.BattleNet.Desktop.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
         public string Greeting => "主页";
+
+        public MainWindowViewModel()
+        {
+            var liveContentDirectory = Path.Combine(Environment.CurrentDirectory, "gitCloneTest");
+            var repoURL = "https://gitee.com/RA3BattleNet/LiveContent.git";
+            var cloneOptions = new CloneOptions()
+            {
+                BranchName = "master",
+                Checkout = true,
+            };
+            Repository.Clone(repoURL, liveContentDirectory);
+        }
+        
     }
 }
